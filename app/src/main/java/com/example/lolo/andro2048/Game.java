cc93a336224c55b9a29bdc410b9690fda198f174
lolo <lorine.darnay@gmail.com>
2015-01-04 23:46:01 +0100

    Added game class skeleton

---
+++ app/src/main/java/com/example/lolo/andro2048/Game.java
@@ -0,0 +1,30 @@
+package com.example.lolo.andro2048;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lolo on 1/4/2015.
+ */
+public class Game {
+    ArrayList<Integer> mGame;
+
+    private void startGame() {
+
+    }
+
+    public void collapseHorizontally() {
+
+    }
+
+    public void collapseVertically() {
+        
+    }
+
+    public int getTileNumber(int position) {
+        return 0;
+    }
+
+    public int getSize() {
+        return 16;
+    }
+}
